2b80152986bceb25337a4aa931168669f72ccdaf
pdMa2s <pedroguilhermematos@ua.pt>
2018-03-23 14:05:50 +0000

    load initial work

---
+++ DiscordControler/DiscordControler/Modules/Ping.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordControler.Modules
+{
+    public class Ping : ModuleBase<SocketCommandContext>
+    {
+        //[Command("ping")]
+        public async Task PingAsync()
+        {
+            await ReplyAsync("Ao seu serviço");
+        }
+    }
+}
