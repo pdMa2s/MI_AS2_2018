bdc72fb95bb8b358ac3ed8b843ccdc74ce40b7dd
Andreia Machado <andreiarfmachado4@gmail.com>
2018-05-06 15:09:09 +0100

    gesture modality

---
+++ GestureModality/GestureModality/GestureDetector.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.Kinect;
+using Microsoft.Kinect.VisualGestureBuilder;
+using System;
+using System.Collections.Generic;
+
+namespace GestureModality
+{
+    class GestureDetector : IDisposable
+    {
+        private readonly string gestureDatabase = @"Database\cenas.gbd";
+        private VisualGestureBuilderFrameSource vgbFrameSource;
+        private VisualGestureBuilderFrameReader vgbFrameReader;
+        private readonly string muteGestureName = "";
+        private readonly string deafGestureName = "";
+        private readonly string deleteMessageGestureName = "";
+
+        public GestureDetector(KinectSensor kinectSensor)
+        {
+            if (kinectSensor == null)
+            {
+                throw new ArgumentNullException("kinectSensor");
+            }
+
+            this.vgbFrameSource = new VisualGestureBuilderFrameSource(kinectSensor, 0);
+            this.vgbFrameReader = this.vgbFrameSource.OpenReader();
+
+            if (this.vgbFrameReader != null)
+            {
+                this.vgbFrameReader.IsPaused = true;
+                this.vgbFrameReader.FrameArrived += this.Reader_GestureFrameArrived;
+            }
+
+            using (VisualGestureBuilderDatabase vgbDatabase = new VisualGestureBuilderDatabase(this.gestureDatabase))
+            {
+                this.vgbFrameSource.AddGestures(vgbDatabase.AvailableGestures);
+            }
+
+            // para fazer disable de gestos que não queremos
+            foreach (Gesture gesture in this.vgbFrameSource.Gestures)
+            {
+                if (gesture.Name.Equals(this.deleteMessageGestureName))
+                {
+                    this.vgbFrameSource.SetIsEnabled(gesture, false);
+                }
+            }
+        }
+
+        private void Reader_GestureFrameArrived(object sender, VisualGestureBuilderFrameArrivedEventArgs e)
+        {
+            VisualGestureBuilderFrameReference frameReference = e.FrameReference;
+            using (VisualGestureBuilderFrame frame = frameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    IReadOnlyDictionary<Gesture, DiscreteGestureResult> discreteResults = frame.DiscreteGestureResults;
+
+                    if (discreteResults != null)
+                    {
+                        bool muteGesture = false;
+                        bool deafGesture = false;
+                        bool deleteMessageGesture = false;
+                        double muteGestureConfidence = 0;
+                        double deafGestureConfidence = 0;
+                        double deleteMessageGestureConfidence = 0;
+
+                        foreach (Gesture gesture in this.vgbFrameSource.Gestures)
+                        {
+                            DiscreteGestureResult result = null;
+                            discreteResults.TryGetValue(gesture, out result);
+
+                            if (result != null)
+                            {
+                                if (gesture.Name.Equals(this.muteGestureName))
+                                {
+                                    muteGesture = result.Detected;
+                                    muteGestureConfidence = result.Confidence;
+                                }
+                                else if (gesture.Name.Equals(this.deafGestureName))
+                                {
+                                    deafGesture = result.Detected;
+                                    deafGestureConfidence = result.Confidence;
+                                }
+                                else if (gesture.Name.Equals(this.deleteMessageGestureName))
+                                {
+                                    deleteMessageGesture = result.Detected;
+                                    deleteMessageGestureConfidence = result.Confidence;
+                                }
+                            }
+                        }
+                        Console.WriteLine("Mute: "+muteGesture + " "+ muteGestureConfidence);
+                        Console.WriteLine("Deaf: " + deafGesture + " " + deafGestureConfidence);
+                        Console.WriteLine("Delete Message: " + deleteMessageGesture + " " + deleteMessageGestureConfidence);
+                    }
+                }
+            }
+        }
+
+        public ulong TrackingId
+        {
+            get
+            {
+                return this.vgbFrameSource.TrackingId;
+            }
+
+            set
+            {
+                if (this.vgbFrameSource.TrackingId != value)
+                    this.vgbFrameSource.TrackingId = value;
+            }
+        }
+
+        public bool IsPaused
+        {
+            get
+            {
+                return this.vgbFrameReader.IsPaused;
+            }
+
+            set
+            {
+                if (this.vgbFrameReader.IsPaused != value)
+                    this.vgbFrameReader.IsPaused = value;
+            }
+        }
+
+        // Disposes the VisualGestureBuilderFrameSource and VisualGestureBuilderFrameReader objects
+        public void Dispose()
+        {
+            if (this.vgbFrameReader != null)
+            {
+                this.vgbFrameReader.FrameArrived -= this.Reader_GestureFrameArrived;
+                this.vgbFrameReader.Dispose();
+                this.vgbFrameReader = null;
+            }
+
+            if (this.vgbFrameSource != null)
+            {
+                this.vgbFrameSource.Dispose();
+                this.vgbFrameSource = null;
+            }
+        }
+    }
+}
---
+++ GestureModality/GestureModality/MainWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.ComponentModel;
+using System.Windows;
+
+namespace GestureModality
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged, IDisposable
+    {
+        private KinectSensor kinectSensor;
+        private Body[] bodies;
+        private int activeBodyIndex;
+        private BodyFrameReader bodyFrameReader;
+        private GestureDetector gestureDetector;
+
+        // INotifyPropertyChangedPropertyChanged event to allow window controls to bind to changeable data
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.kinectSensor = KinectSensor.GetDefault();
+            this.kinectSensor.Open();
+
+            this.bodyFrameReader = this.kinectSensor.BodyFrameSource.OpenReader();
+            this.bodyFrameReader.FrameArrived += this.Reader_BodyFrameArrived;
+
+            this.gestureDetector = new GestureDetector(kinectSensor);
+            this.activeBodyIndex = -1;
+
+        }
+
+        private void Reader_BodyFrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            bool dataReceived = false;
+
+            using (BodyFrame bodyFrame = e.FrameReference.AcquireFrame())
+            {
+                if (bodyFrame != null)
+                {
+                    if (bodies == null)
+                    {
+                        // creates an array of 6 bodies, which is the max number of bodies that Kinect can track simultaneously
+                        this.bodies = new Body[bodyFrame.BodyCount];
+                    }
+                    // The first time GetAndRefreshBodyData is called, Kinect will allocate each Body in the array.
+                    // As long as those body objects are not disposed and not set to null in the array,
+                    // those body objects will be re-used.
+                    bodyFrame.GetAndRefreshBodyData(this.bodies);
+                    verifyActiveBody();
+                    dataReceived = true;
+                }
+            }
+
+            if (dataReceived)
+            {
+                Body body = this.bodies[this.activeBodyIndex];
+
+                // if the current body TrackingId changed, update the corresponding gesture detector with the new value
+                if (body.TrackingId != this.gestureDetector.TrackingId)
+                    this.gestureDetector.TrackingId = body.TrackingId;
+
+                // if the current body is tracked, unpause its detector to get VisualGestureBuilderFrameArrived events
+                // if the current body is not tracked, pause its detector so we don't waste resources trying to get invalid gesture results
+                this.gestureDetector.IsPaused = (body.TrackingId == 0);
+            }
+
+        }
+
+        private void verifyActiveBody()
+        {
+            if (this.activeBodyIndex != -1)
+            {
+                Body body = this.bodies[this.activeBodyIndex];
+                if (!body.IsTracked)
+                {
+                    this.activeBodyIndex = -1;
+                }
+            }
+
+            if (this.activeBodyIndex == -1)
+            {
+                int maxBodies = this.kinectSensor.BodyFrameSource.BodyCount;
+
+                for (int i=0; i < maxBodies; i++)
+                {
+                    // find the first tracked body
+                    if (this.bodies[i].IsTracked)
+                    {
+                        this.activeBodyIndex = i;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Disposes the GestureDetector object
+        // True if Dispose was called directly, false if the GC handles the disposing
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (this.gestureDetector != null)
+                {
+                    this.gestureDetector.Dispose();
+                    this.gestureDetector = null;
+                }
+            }
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (this.bodyFrameReader != null)
+            {
+                this.bodyFrameReader.FrameArrived -= this.Reader_BodyFrameArrived;
+                this.bodyFrameReader.Dispose();
+                this.bodyFrameReader = null;
+            }
+
+            if (this.gestureDetector != null)
+            {
+                this.gestureDetector.Dispose();
+                this.gestureDetector = null;
+            }
+
+            if (this.kinectSensor != null)
+            {
+                this.kinectSensor.Close();
+                this.kinectSensor = null;
+            }
+
+        }
+    }
+}
